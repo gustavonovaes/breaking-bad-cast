e0f3c1bc41e2d496888887b2d2e1401352281166
Gustavo Novaes <gustavonovaes93@gmail.com>
2020-07-22 10:11:55 -0300

    Updates

---
+++ precache-manifest.922ab9c0c2fbef29baf44d508e7dccbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642194e40d7ff74509749ec5b6760cb3",
+    "url": "/gustavonovaes/breaking-bad-cast/index.html"
+  },
+  {
+    "revision": "8a62aac83ecec1ecb82a",
+    "url": "/gustavonovaes/breaking-bad-cast/static/css/main.8e8b834b.chunk.css"
+  },
+  {
+    "revision": "544608dd38c62ab64354",
+    "url": "/gustavonovaes/breaking-bad-cast/static/js/2.8aa8be2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gustavonovaes/breaking-bad-cast/static/js/2.8aa8be2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a62aac83ecec1ecb82a",
+    "url": "/gustavonovaes/breaking-bad-cast/static/js/main.6d9f70aa.chunk.js"
+  },
+  {
+    "revision": "b5e0d101fea68b4597a4",
+    "url": "/gustavonovaes/breaking-bad-cast/static/js/runtime-main.7f94c8c1.js"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/gustavonovaes/breaking-bad-cast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/gustavonovaes/breaking-bad-cast/static/media/spinner.235b9b3e.gif"
+  }
+]);
